e020d61ad1fc37b8464266c0abdb381ac9bd98c1
Aleksander Dariusz Świniarski <a.swiniarski@stud.elka.pw.edu.pl>
2020-05-15 15:54:07 +0200

    Update Common.h

--- src/Amazons_V.8.0/headers/Common.h
+++ src/Amazons_V.8.0/headers/Common.h
@@ -14,83 +14,67 @@
 #define CTRL_MOVE 5
 #define Starting_position 0
 
+/**
+* A structure with date of a tile
+*/
 typedef struct tile {
-    int treasure;
-    int artifact;
-    int occupation;
+    /*@{*/
+    int treasure;   /**< value of treasure on tile          */
+    int artifact;   /**< value of artifact on tile          */
+    int occupation; /**< ID of players/element on a tile    */
+    /*@}*/
 }tile;
 
+/**
+* A structure defining a information of each player
+*/
 typedef struct player_data {
-    char name[NAME_LENGTH];
-    int ID;
-    int points;
+    /*@{*/
+    char name[NAME_LENGTH]; /**< Name of player (Maximal lenght of 15)      */
+    int ID;                 /**< Id of player                               */
+    int points;             /**< Points of earned by player                 */
+    /*@}*/
 }player_data;
 
+/**
+* A structure defining a coordinate of a point
+*/
 typedef struct coordinate {
-    int x;
-    int y;
+    /*@{*/
+    int x;  /**< x coordinate of the point  */
+    int y;  /**< y coordinate of the point  */
+    /*@}*/
 }coordinate;
 
+/**
+* A structure defining a data fixed by players
+*/
 typedef struct fixed_inf{
-    int width;
-    int height;
-    int number_of_players;
-    int number_of_pawns;
+    /*@{*/
+    int width;              /**< width of the board                 */
+    int height;             /**< height of the board                */
+    int number_of_players;  /**< how many players play the game     */
+    int number_of_pawns;    /**< how many pawns each players have   */
+    /*@}*/
 }fixed_inf;
 
-typedef struct game_state {
-    tile** board;
-    player_data* player_list;
-    coordinate point_1;
-    coordinate point_2;
-    fixed_inf fixed;
-    int current_player;
-    int n_player;
-    int p_with_valid_moves;
-    int* still_in_game;
-    int c;
-}game_state;
-/*
-        DATA STRUCTURE OF THE GAME
-
-    TILE:   (Structure defining each tile of the board)
-
-        int treasure    -   value of treasure on tile
-        int artifact    -   value of artifact on a tile
-        int occupation  -   ID of players/element on a tile
-
-    PLAYER_DATA: (Structure defining a information of each player)
-
-        char name[] -   Name of player 
-        int ID      -   Id of this player
-        int points  -   Points of this player
-
-    COORDINATE: (Structure defining a coordinate of a point)
-
-        x - x coordinate of the point
-        y - y coordinate of the point
-
-    FIXED_INF:   (Structure defining a data fixed by players)
-
-        int width               - width of the board
-        int height              - height of the board
-        int number_of_players   - how many players play the game
-        int number_of_pawns     - how many pawns each players have
-
-    GAME_STATE: (Whole strcutre of the game)
-
-        tile** board                - array of tiles (board)
-        player_data* player_list    - list of players
-        coordinate point_1          - coordinates of a point 1
-        coordinate point_2          - coordinates of a point 2
-        fixed_inf fixed             - information fixed by player
-        int current_player          - id of player currently making move
-        int n_player                - n-th player
-        int p_with_valid_moves      - how many players still can make a move
-        int still_in_game           - how many players are still playing
-        int c                       - currently stored character
-
+/**
+* A structure describing the state of the game
 */
+typedef struct game_state {
+    /*@{*/
+    tile** board;               /**< array of tiles (board)                     */
+    player_data* player_list;   /**< list of players                            */
+    coordinate point_1;         /**< coordinates of a point 1                   */
+    coordinate point_2;         /**< coordinates of a point 2                   */
+    fixed_inf fixed;            /**< information fixed by player                */
+    int current_player;         /**< id of player currently making move         */
+    int n_player;               /**< n-th player                                */
+    int p_with_valid_moves;     /**< how many players still can make a move     */
+    int* still_in_game;         /**< how many players are still playing         */
+    int c;                      /**< currently stored character                 */
+    /*@}*/
+}game_state;
 
 void blank_table(int table, int size);
 /** 
