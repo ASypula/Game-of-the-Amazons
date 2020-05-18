38dc7424c5ff752408a4fdc99d76599dfe59e7c2
Aleksander Dariusz Świniarski <a.swiniarski@stud.elka.pw.edu.pl>
2020-05-18 16:03:58 +0200

    Upload New File

---
+++ Autonomous version/Headers/src_Amazons_V.8.0_headers_Output.h
@@ -0,0 +1,57 @@
+#ifndef _Output_H
+#define _Output_H
+
+#include "Common.h"
+
+void generate_board(struct game_state* GS);
+/**
+* A function fills each tile of the board with random treasure and artifact
+* @param *GS - game_state
+*/
+
+
+//Function drawing given board with colors
+void draw_board_color(struct game_state* GS);
+/**
+* A function draws a board using colors
+* @param *GS - game_state
+*/
+
+void order_scores(int number_of_players, player_data player[], player_data ranking[]);
+/*
+* Creates a new structure by copying the player structure
+* sorts a new structure by the number of points
+* @param number_of_players - number of players in the game
+* @param player[] - old structure
+* @param ranking - new strucutre
+*/
+
+void add_player_data_file(FILE* fp, struct game_state* GS);
+/*
+* Saves in file data about player_data
+* @param *fp - file pointer
+* @param *GS - game_state
+*/
+
+void add_size_file(FILE* fp, struct game_state* GS);
+/*
+* Saves in file data about size
+* @param *fp - file pointer
+* @param *GS - game_state
+*/
+
+void add_tiles_file(FILE* fp, struct game_state* GS);
+/*
+* Saves in file data about files
+* @param *fp - file pointer
+* @param *GS - game_state
+*/
+
+void save_data_file(char* file_name, struct game_state* GS);
+/*
+* Saves in file stored data from a game_state
+* @param *file_name - name of the file
+* @param *GS - game_state
+*/
+
+#endif // _Output_H
