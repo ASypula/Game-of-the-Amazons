a48a6c7fa9638af1532455ed4e667b0560dcf41d
Aleksandra Anna Sypuła <a.sypula@stud.elka.pw.edu.pl>
2020-05-31 17:42:12 +0200

    Update Amazons_auto_Headers_Input.h

--- Autonomous version/Headers/Amazons_auto_Headers_Input.h
+++ Autonomous version/Headers/Amazons_auto_Headers_Input.h
@@ -6,10 +6,11 @@
 
 void place_amazons(game_state* GS);
 /**
-* place amazons on the board
+* places one amazon on the board
 * deletes artifacts and points from chosen tiles
 * changes occupation for player ID number in structure tile board
 * @param *GS - game_state
+* @return 1 if amazon was placed, 0 if amazon cannot be placed on the board
 */
 
 
@@ -65,7 +66,7 @@ int found_horse(game_state* GS, int *dx, int *dy, int x, int y);
 * @return 0 - if there is no horse on the board, 1 - if there is a horse
 */
 
-int max_point (game_state* GS, int *dx, int*dy, int x, int y);
+int max_point (game_state* GS, int *dx, int*dy, int x, int y, int search_max);
 /**
 * Function finds a tile with maximum number of points (treasure)
 * @param *GS - game_state
@@ -74,16 +75,32 @@ int max_point (game_state* GS, int *dx, int*dy, int x, int y);
 * @param x - x coordinate from which function is supposed to start checking
 * @param y - y coordinate from which function is supposed to start checking
 * (x, y) - starting position
-* @return maximum number of found treasure
+* @param search_max - maximum value of treasure that may still be available for an amazon (that is yet to be checked) 
+* @return 1 if a tile with search_max treasure was found, 0 if it wasn't
 */
 
-int tile_available (game_state* GS, int x, int y);
+int tile_available (game_state* GS, int x, int y, int *found_x, int *found_y);
 /**
 * Function checks if a tile is available for an amazon positioned next to it
 * @param *GS - game_state
 * @param x - x coordinate of the tile being checked
 * @param y - y coordinate of the tile being checked
+* @param *found_x - pointer to x coordinate
+* @param *found_y - pointer to y coordinate
+* (*found_x, *found_y) - a tile chosen to move the amazon
 * @return 0 - if the tile is not available, 1 - if the tile is available
+*/
+
+int best_place (game_state* GS, int *found_x, int *found_y);
+/**
+* Function searches for the best tile to move the amazon
+* first looks for a tile close to a horse
+* second looks for a tile with the highest treasure
+* @param *GS - game_state
+* @param *found_x - pointer to x coordinate
+* @param *found_y - pointer to y coordinate
+* (*found_x, *found_y) - a tile chosen to move the amazon
+* @return 1 - if a tile to move an amazon was found, 0 - if no amazon can be placed on the board
 */
 
 #endif // _Input_H
