9b3de5c54db4f06d88f084bbe0a2e113cc47ac03
Aleksandra Anna Sypuła <a.sypula@stud.elka.pw.edu.pl>
2020-06-03 21:02:05 +0200

    Update Amazons_auto_Source_Common.c

--- Autonomous version/Source/Amazons_auto_Source_Common.c
+++ Autonomous version/Source/Amazons_auto_Source_Common.c
@@ -8,23 +8,14 @@ int is_digit (char c)
         return 0;
 }
 
-int get_next(game_state* GS)
+int find_ID (game_state* GS)
 {
-    int p = 0;
-    while (GS->c != ' ')
+    int i = 0;
+    while (strcmp(GS->player_list[i].name, GS->name))
     {
-        p = 10 * p + number(GS->c);
-        GS->c = getchar();
+        i++;
     }
-    while (GS->c == ' ' || GS->c == '\t' || GS->c == '\n' || GS->c == EOF) //what about boards with more than 10? taking a tile 11, 11
-    {
-        if (GS->c == EOF)
-        {
-            // what then?
-        }
-        GS->c = getchar();
-    }
-    return p;
+    return GS->player_list[i].ID;
 }
 
 int number(int x)
@@ -46,4 +37,18 @@ int fmax_of8 (int x1, int x2, int x3, int x4, int x5, int x6, int x7, int x8)
     int max_x;
     max_x = find_max(find_max(find_max(x1, x2), find_max(x3, x4)), find_max(find_max(x5, x6), find_max(x7, x8)));
     return max_x;
+}
+
+void free_memory (game_state* GS)
+{
+    int p = 0;
+    //Freeing the memory
+    while (p < GS->fixed.height) {
+        free(*(GS->board + p));
+        p++;
+    }
+    free(GS->board);
+    free(GS->player_list);
+    free(GS->positions);
+    free(GS);
 }
