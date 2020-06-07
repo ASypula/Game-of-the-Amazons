4a4510e9354d75d224ba51d2d81571471e7edd9b
Aleksander Dariusz Świniarski <a.swiniarski@stud.elka.pw.edu.pl>
2020-06-07 23:13:52 +0200

    Update Amazons_auto_Project.c

--- Autonomous version/Amazons_auto_Project.c
+++ Autonomous version/Amazons_auto_Project.c
@@ -9,8 +9,7 @@
 
 int main(int argc, char *argv[]) {
 
-    //Declaring structure and first zero values
-
+    //Game state
     game_state state;
     game_state* GS = &state;
     GS->name = (char*) malloc (NAME_LENGTH * sizeof(char));
@@ -20,6 +19,50 @@ int main(int argc, char *argv[]) {
     state.current_player = 0;
     state.n_player = 0;
     state.fixed.number_of_pawns = 0;
+
+    //Ac system
+    game_state check;
+    game_state* TEST = &check;
+    TEST->name = (char*) malloc (NAME_LENGTH * sizeof(char));
+    TEST->name = "DoubleA";
+
+    check.error = 0;
+    check.current_player = 0;
+    check.n_player = 0;
+    check.fixed.number_of_pawns = 0;
+    
+    #ifdef RUN_UNIT_TEST
+
+    //Testing of reading file - TEST.txt
+
+    game_state u_test;
+    game_state* UT = &u_test;
+    UT->name = (char*) malloc (NAME_LENGTH * sizeof(char));
+    UT->name = "DoubleA";
+
+    u_test.error = 0;
+    u_test.current_player = 0;
+    u_test.n_player = 0;
+    u_test.fixed.number_of_pawns = 0;
+
+    read_file("TEST.txt", UT);
+
+    test(TRUE, UT->fixed.height==5, "Read_height");
+    test(TRUE, UT->fixed.width==6, "Read_width");
+    test(TRUE, UT->board[4][0].occupation==5, "Read_occupation");
+    test(TRUE, UT->board[2][5].treasure==3, "Read_treasure");
+    test(TRUE, UT->board[0][2].artifact==2, "Read_artifact");
+    test(TRUE, UT->fixed.number_of_players==5, "Read_number_of_players");
+    test(TRUE, !strcmp("DoubleA", UT->player_list[0].name), "Read_name");
+    test(TRUE, UT->player_list[4].ID==5, "Read_ID");
+    test(TRUE, UT->player_list[1].points==38, "Read_points");
+    test(TRUE, find_ID(UT)==1, "Find_our_ID");
+    test(TRUE, fmax_of8(-12, 0, 3, 4, 5, 87, 256, 19)==256, "Find_max_of_eight");
+    test(FALSE, is_occupied(UT, 2, 2), "Check_if_occupied");
+    test(FALSE, tile_with_enemy(UT, 0, 0), "Check_tile_with_enemy");
+
+    #endif // RUN_UNIT_TEST
+    
 
     if (argc == 5 && !strcmp(argv[1], "phase=placement")) // four parameters, placement phase
     {
@@ -66,21 +109,31 @@ int main(int argc, char *argv[]) {
         //outputfile_name = argv[3];
 
         read_file(argv[2], GS);
+        read_file("AC.txt", TEST);
 
         if (state.error == 1) {
             return 2;
         }
         else {
+            
+            //AC SYSTEM
+            if(!TEST->error){
+            ac_system(GS,TEST);
+            }
+            
             if (move_amazon(GS))
             {
                 save_data_file(argv[3], GS);
+                save_data_file("AC.txt", GS);
                 free_memory(GS);
+                free_memory(TEST);
                 return 0;
             }
             else
             {
                 printf("All amazons are blocked\n");
                 free_memory(GS);
+                free_memory(TEST);
                 return 1;
             }
         }
